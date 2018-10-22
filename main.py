06c7c231c59775677a1dd652820defd2c9da01a9
Hiroki Sakuma <hirokisakuma@HirokinoMacBook-puro.local>
2018-10-22 22:00:21 +0900

    update

---
+++ main.py
@@ -0,0 +1,144 @@
+import tensorflow as tf
+import numpy as np
+import argparse
+import ops
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--steps", type=int, default=10000, help="number of training steps")
+parser.add_argument("--num_epochs", type=int, default=100, help="number of training epochs")
+parser.add_argument("--batch_size", type=int, default=100, help="batch size")
+parser.add_argument("--model_dir", type=str, default="mnist_cnn_model", help="model directory")
+parser.add_argument('--train', action="store_true", help="with training")
+parser.add_argument('--eval', action="store_true", help="with evaluation")
+parser.add_argument('--predict', action="store_true", help="with prediction")
+parser.add_argument('--gpu', type=str, default="0", help="gpu id")
+args = parser.parse_args()
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+def capsule_network_model_fn(features, labels, mode, params):
+
+    inputs = features["images"]
+
+    inputs = tf.layers.flatten(inputs)
+
+    inputs = tf.expand_dims(inputs, axis=-1)
+
+    inputs = ops.capsule_dense(
+        inputs=inputs,
+        units=128,
+        dims=8,
+        name="capsule_dense_0"
+    )
+
+    inputs = ops.capsule_dense(
+        inputs=inputs,
+        units=128,
+        dims=16,
+        name="capsule_dense_1"
+    )
+
+    inputs = ops.capsule_dense(
+        inputs=inputs,
+        units=10,
+        dims=32,
+        name="capsule_dense_2"
+    )
+
+    logits = tf.norm(inputs, axis=-1)
+    labels = tf.one_hot(labels, 10)
+
+    positive_loss = tf.square(tf.maximum(0.0, 0.9 - logits))
+    positive_loss = tf.reduce_mean(tf.reduce_sum(positive_loss * (labels - 0), axis=-1))
+
+    negative_loss = tf.square(tf.maximum(0.0, logits - 0.1))
+    negative_loss = tf.reduce_mean(tf.reduce_sum(negative_loss * (1 - labels), axis=-1))
+
+    loss = positive_loss * 1.0 + negative_loss * 0.5
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+
+        with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+
+            train_op = tf.train.AdamOptimizer().minimize(
+                loss=loss,
+                global_step=tf.train.get_global_step()
+            )
+
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            train_op=train_op
+        )
+
+    if mode == tf.estimator.ModeKeys.EVAL:
+
+        eval_metric_ops = {
+            "accuracy": tf.metrics.accuracy(
+                labels=labels,
+                predictions=tf.argmax(logits, axis=-1)
+            )
+        }
+
+        return tf.estimator.EstimatorSpec(
+            mode=mode,
+            loss=loss,
+            eval_metric_ops=eval_metric_ops
+        )
+
+
+def main(unused_argv):
+
+    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+    train_images = mnist.train.images.astype(np.float32)
+    test_images = mnist.test.images.astype(np.float32)
+    train_labels = mnist.train.labels.astype(np.int32)
+    test_labels = mnist.test.labels.astype(np.int32)
+
+    mnist_classifier = tf.estimator.Estimator(
+        model_fn=capsule_network_model_fn,
+        model_dir=args.model_dir,
+        config=tf.estimator.RunConfig().replace(
+            session_config=tf.ConfigProto(
+                gpu_options=tf.GPUOptions(
+                    visible_device_list=args.gpu,
+                    allow_growth=True
+                )
+            )
+        )
+    )
+
+    if args.train:
+
+        train_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x={"images": train_images},
+            y=train_labels,
+            batch_size=args.batch_size,
+            num_epochs=args.num_epochs,
+            shuffle=True
+        )
+
+        mnist_classifier.train(
+            input_fn=train_input_fn
+        )
+
+    if args.eval:
+
+        eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+            x={"images": test_images},
+            y=test_labels,
+            batch_size=args.batch_size,
+            num_epochs=1,
+            shuffle=False
+        )
+
+        eval_results = mnist_classifier.evaluate(
+            input_fn=eval_input_fn
+        )
+
+        print(eval_results)
+
+
+if __name__ == "__main__":
+    tf.app.run()
---
+++ ops.py
@@ -0,0 +1,127 @@
+import tensorflow as tf
+import numpy as np
+
+
+def squash(inputs, axis):
+
+    norm = tf.norm(inputs, axis=axis, keep_dims=True)
+    squared_norm = tf.square(norm)
+
+    return squared_norm / (1. + squared_norm) / norm * inputs
+
+
+def element_wise_matmul(a, b):
+
+    l, m = a.shape.as_list()[-2:]
+    m, n = b.shape.as_list()[-2:]
+
+    s = a.shape.as_list()
+    d = len(s)
+
+    a = tf.tile(a, [1] * (d - 1) + [n])
+    a = tf.reshape(a, [-1] + s[1:-2] + [l * n, m])
+
+    b = tf.tile(b, [1] * (d - 1) + [l])
+    b = tf.transpose(b, range(d - 2) + [d - 1, d - 2])
+
+    c = a * b
+    c = tf.reduce_sum(c, axis=-1)
+    c = tf.reshape(c, [-1] + s[1:-2] + [l, n])
+
+    return c
+
+
+def capsule_dense(inputs, units, dims, num_iters=3, name="capsule_dense", reuse=None):
+
+    with tf.variable_scope(name, reuse=reuse):
+
+        in_units = inputs.shape[1]
+        in_dims = inputs.shape[2]
+
+        weights = tf.get_variable(
+            name="weights",
+            shape=[in_units, units, in_dims, dims],
+            dtype=tf.float32,
+            initializer=tf.variance_scaling_initializer(
+                scale=2.0,
+                mode="fan_in",
+                distribution="normal",
+            ),
+            trainable=True
+        )
+
+        biasses = tf.get_variable(
+            name="biasses",
+            shape=[units, dims],
+            initializer=tf.zeros_initializer(),
+            trainable=True
+        )
+
+        # -> inputs: [batch_size, in_units, in_dims]
+        inputs = tf.tile(inputs, [1, units, 1])
+        # -> inputs: [batch_size, in_units * out_units, in_dims]
+        inputs = tf.reshape(inputs, [-1, in_units, units, in_dims])
+        # -> inputs: [batch_size, in_units, out_units, in_dims]
+        inputs = tf.expand_dims(inputs, axis=-2)
+        # -> inputs: [batch_size, in_units, out_units, 1, in_dims]
+
+        # -> weights: [in_units, out_units, in_dims, out_dims]
+        weights = tf.expand_dims(weights, axis=0)
+        # -> weights: [1, in_units, out_units, in_dims, out_dims]
+
+        # -> inputs: [batch_size, in_units, out_units, 1, in_dims]
+        inputs = element_wise_matmul(inputs, weights)
+        # -> inputs: [batch_size, in_units, out_units, 1, out_dims]
+        inputs = tf.squeeze(inputs, axis=-2)
+        # -> inputs: [batch_size, in_units, out_units, out_dims]
+
+        inputs_fixed = tf.stop_gradient(inputs)
+
+        logits = tf.constant(
+            value=0.0,
+            dtype=tf.float32,
+            shape=[in_units, units]
+        )
+
+        for i in range(num_iters):
+
+            softmax = tf.nn.softmax(logits, axis=0)
+            # -> softmax: [in_units, out_units]
+            softmax = tf.expand_dims(softmax, axis=-1)
+            # -> softmax: [in_units, out_units, 1]
+
+            outputs = inputs_fixed * softmax
+            # -> outputs: [batch_size, in_units, out_units, out_dims]
+            outputs = tf.reduce_sum(outputs, axis=1, keep_dims=True)
+            # -> outputs: [batch_size, 1, out_units, out_dims]
+            outputs += biasses
+            # -> outputs: [batch_size, 1, out_units, out_dims]
+            outputs = squash(outputs, axis=-1)
+            # -> outputs: [batch_size, 1, out_units, out_dims]
+
+            similarity = inputs_fixed * outputs
+            # -> similarity: [batch_size, in_units, out_units, out_dims]
+            similarity = tf.reduce_sum(similarity, axis=-1)
+            # -> similarity: [batch_size, in_units, out_units]
+            similarity = tf.reduce_mean(similarity, axis=0)
+            # -> similarity: [in_units, out_units]
+            logits += similarity
+
+        else:
+
+            softmax = tf.nn.softmax(logits, axis=0)
+            # -> softmax: [in_units, out_units]
+            softmax = tf.expand_dims(softmax, axis=-1)
+            # -> softmax: [in_units, out_units, 1]
+
+            outputs = inputs * softmax
+            # -> outputs: [batch_size, in_units, out_units, out_dims]
+            outputs = tf.reduce_sum(outputs, axis=1, keep_dims=True)
+            # -> outputs: [batch_size, 1, out_units, out_dims]
+            outputs += biasses
+            # -> outputs: [batch_size, 1, out_units, out_dims]
+            outputs = squash(outputs, axis=-1)
+            # -> outputs: [batch_size, 1, out_units, out_dims]
+            outputs = tf.squeeze(outputs, axis=1)
+
+        return outputs
